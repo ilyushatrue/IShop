f397be9b6196e36aaa729c6b2cfe766558668bdb
itruhin <i.truhin@cmachine.ru>
2024-08-24 18:12:04 +1000

    wip

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,6 +1,11 @@
 import "../assets/fonts/fonts.css";
 import "../extension-methods/array.extensions";
-import { CssBaseline, GlobalStyles, ThemeProvider, useMediaQuery } from "@mui/material";
+import {
+	CssBaseline,
+	GlobalStyles,
+	ThemeProvider,
+	useMediaQuery,
+} from "@mui/material";
 import { theme } from "../styles/theme-provider";
 import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
@@ -11,26 +16,18 @@ import { PopupProvider } from "../components/popup";
 import { MediaQueryProvider } from "./infrastructure/media-query-context";
 
 function App() {
-	const sm = useMediaQuery("(min-width:600px)");
 	return (
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<GlobalStyles
 					styles={{
-						html: {
-							backgroundColor: "white", // Белый фон для html
-							height: "100%",
-						},
-						body: {
-							backgroundColor: "white", // Белый фон для body
+						"html, body, .MuiPaper-root": {
+							backgroundColor: "white",
 							height: "100%",
 							margin: 0,
 							padding: 0,
-							overflowX: "hidden", // Скрыть горизонтальный скролл
-						},
-						"#root": {
-							height: "100%", // Убедиться, что root занимает всю высоту
+							overflowX: "hidden", // Скрыть горизонтальную прокрутку
 						},
 					}}
 				/>
